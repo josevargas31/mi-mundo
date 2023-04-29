bfb312b28fe356db1e60aac3f5a2dce5ef5155dd
josevargas31 <dajozavargas@gmail.com>
2023-04-28 20:58:17 -0500

    add fuentes

--- js/index.js
+++ js/index.js
@@ -8,6 +8,6 @@ document.getElementById("loading-message").innerText = "Enviado!"
 
 
 setTimeout(function () {
-    alert("recibiste un abrazo! Un abrazo sincero es la mejor manera de decir cuanto lo siento...")
+    alert("recibiste un abrazo! Solo quiero dar lo último de mí, lo siento, sip, ni aquello ni lo otro… esta bien, me deja muy claro que ya no quieres que yo sea parte de tu vida. Pero no me resistí de darte algo, le dedique buen tiempo… bueno tenía algo más, pero decidí darte este pedacito de mi… Espero que disfrutes tu día y vida al máximo¡! ")
 }, 3500)
 
