c953e064da3f936e99676e7d8f7bc88e208fb737
josevargas31 <dajozavargas@gmail.com>
2023-04-28 19:17:32 -0500

    add fuentes

---
+++ js/abrazo.js
@@ -0,0 +1,13 @@
+
+setTimeout(cambiamensaje, 3300)
+
+function cambiamensaje() {
+document.getElementById("loading-message").innerText = "Enviado!"
+}
+
+
+
+setTimeout(function () {
+    alert("recibiste un abrazo! Un abrazo sincero es la mejor manera de decir cuanto lo siento...")
+}, 3500)
+
